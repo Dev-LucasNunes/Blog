00a0678d8f5a2f225e323f79bd1cd11edc4e784c
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-04-17 18:29:24 -0300

    project complete

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,6 +3,6 @@ class BlogController < ApplicationController
   layout "application_blank"
   # para todas as açoes vai usar esse blak
   def index
-    @post = Post.where('publish_date <= ?', Time.zone.now).first
+    @post = Post.where('publish_date <= ?', Time.zone.now).order("publish_date desc").first
   end
 end
