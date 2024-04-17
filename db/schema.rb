51357e185bf6ffbfcf86a141e6092187530e3b83
Dev-LucasNunes <dev.lucasnunes@gmail.com>
2024-04-17 17:05:29 -0300

    feat: admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_16_211835) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_17_195405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.string "title", limit: 150
     t.text "body"
-    t.datetime "data_publicacao"
+    t.datetime "publish_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
